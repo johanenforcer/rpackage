c2f271837090322315af538212b01aa48538b9a8
Johanz <random@gmail.com>
2021-09-10 01:08:23 +0200

    added wiki_graph stored as data.rda for storing R objects as data files

---
+++ lab3package/R/data.R
@@ -0,0 +1,11 @@
+#' Wiki graph data
+#'
+#' @format A data frame, 18 x 3
+#' \describe{
+#'   \item{v1}{from node}
+#'   \item{v2}{to node}
+#'   \item{w}{weight for edge between nodes}
+#' }
+#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#' 
+"wiki_graph"
---
+++ lab3package/R/lab3package.R
@@ -0,0 +1,8 @@
+#' lab3package
+#' 
+#' @docType package
+#' @author Johan Marbinah
+#' @name lab3package
+#' @description Solutions for euclidean and dijkstras algorithms ...
+#' 
+NULL
