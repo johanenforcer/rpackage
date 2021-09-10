62047b7af5f5baaeded523968b5014039f9e8315
Johanz <random@gmail.com>
2021-09-10 13:19:15 +0200

    added source to wikigraph r documenation file through roxygen2

--- lab3package/R/data.R
+++ lab3package/R/data.R
@@ -2,10 +2,10 @@
 #'
 #' @format A data frame, 18 x 3
 #' \describe{
-#'   \item{v1}{from node}
+#'   \item{v1}{from node}s
 #'   \item{v2}{to node}
 #'   \item{w}{weight for edge between nodes}
 #' }
-#' @source \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
+#' @source https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 #' 
 "wiki_graph"
